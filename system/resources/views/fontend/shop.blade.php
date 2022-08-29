203fb834252fff61f6f969831c1fa44ba41dba3c
IrsyadHusainJauhari123 <108142445+IrsyadHusainJauhari123@users.noreply.github.com>
2022-08-29 23:57:08 +0700

    Update shop.blade.php

--- system/resources/views/fontend/shop.blade.php
+++ system/resources/views/fontend/shop.blade.php
@@ -131,115 +131,117 @@
     <!-- products -->
     <div class="product-section mt-150 mb-150">
         <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-filters">
+                        <ul>
+                            <li class="active" data-filter="*">All</li>
+                            <li data-filter=".strawberry">Strawberry</li>
+                            <li data-filter=".berry">Berry</li>
+                            <li data-filter=".lemon">Lemon</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row product-lists">
+                <div class="col-lg-4 col-md-6 text-center strawberry">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-1.jpg" alt=""></a>
+                        </div>
+                        <h3>Strawberry</h3>
+                        <p class="product-price"><span>Per Kg</span> 85$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center berry">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-2.jpg"
+                                    alt=""></a>
+                        </div>
+                        <h3>Berry</h3>
+                        <p class="product-price"><span>Per Kg</span> 70$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center lemon">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-3.jpg"
+                                    alt=""></a>
+                        </div>
+                        <h3>Lemon</h3>
+                        <p class="product-price"><span>Per Kg</span> 35$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-4.jpg"
+                                    alt=""></a>
+                        </div>
+                        <h3>Avocado</h3>
+                        <p class="product-price"><span>Per Kg</span> 50$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-5.jpg"
+                                    alt=""></a>
+                        </div>
+                        <h3>Green Apple</h3>
+                        <p class="product-price"><span>Per Kg</span> 45$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center strawberry">
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <a href="{{ url('single') }}"><img
+                                    src="{{ url('public') }}/assets/img/products/product-img-6.jpg"
+                                    alt=""></a>
+                        </div>
+                        <h3>Strawberry</h3>
+                        <p class="product-price"><span>Per Kg</span> 80$ </p>
+                        <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
+                            to
+                            Cart</a>
+                    </div>
+                </div>
+            </div>
 
-            <form action="{{ url('/shop/filter') }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="" class="control-label">Nama produk</label>
-                    <input type="text" class="form-control" name="nama" value="{{ $nama ?? '' }}">
-                </div>
-                <br>
-                <button class="btn btn-dark float-right"></i>Filter</button>
-                <div class="row product-lists">
-                    <div class="col-lg-4 col-md-6 text-center strawberry">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-1.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Strawberry</h3>
-                            <p class="product-price"><span>Per Kg</span> 85$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center berry">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-2.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Berry</h3>
-                            <p class="product-price"><span>Per Kg</span> 70$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center lemon">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-3.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Lemon</h3>
-                            <p class="product-price"><span>Per Kg</span> 35$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-4.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Avocado</h3>
-                            <p class="product-price"><span>Per Kg</span> 50$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-5.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Green Apple</h3>
-                            <p class="product-price"><span>Per Kg</span> 45$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center strawberry">
-                        <div class="single-product-item">
-                            <div class="product-image">
-                                <a href="{{ url('single') }}"><img
-                                        src="{{ url('public') }}/assets/img/products/product-img-6.jpg"
-                                        alt=""></a>
-                            </div>
-                            <h3>Strawberry</h3>
-                            <p class="product-price"><span>Per Kg</span> 80$ </p>
-                            <a href="{{ url('cart') }}" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add
-                                to
-                                Cart</a>
-                        </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="pagination-wrap">
+                        <ul>
+                            <li><a href="#">Prev</a></li>
+                            <li><a href="#">1</a></li>
+                            <li><a class="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">Next</a></li>
+                        </ul>
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <div class="pagination-wrap">
-                            <ul>
-                                <li><a href="#">Prev</a></li>
-                                <li><a href="#">1</a></li>
-                                <li><a class="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">Next</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+            </div>
         </div>
     </div>
     <!-- end products -->
